f477749906b53f4d8145d51e88cfb9adbb2ec402
moses wambui <moses.wambui@smart.local>
2023-11-04 00:54:09 +0300

    Book-Author-Library-categories-UserProfile-Reviews

---
+++ src/main/java/com/novelsMDW/Novel/Entities/Author.java
@@ -0,0 +1,4 @@
+package com.novelsMDW.Novel.Entities;
+
+public class Author {
+}
---
+++ src/test/java/com/novelsMDW/Novel/NovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novelsMDW.Novel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
