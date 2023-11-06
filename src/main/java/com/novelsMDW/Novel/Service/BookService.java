5ffa41470d52b5bd7ec3112ec8397c5e58c679c0
moses wambui <moses.wambui@smart.local>
2023-11-07 00:04:57 +0300

    Trying to merge develop branch To ft-native-queries BookService.java file Error

--- src/main/java/com/novelsMDW/Novel/Service/BookService.java
+++ src/main/java/com/novelsMDW/Novel/Service/BookService.java
@@ -3,29 +3,24 @@ package com.novelsMDW.Novel.Service;
 import com.novelsMDW.Novel.Entities.Author;
 import com.novelsMDW.Novel.Entities.Book;
 import com.novelsMDW.Novel.Entities.Category;
-import com.novelsMDW.Novel.Exception.AuthorNotFoundException;
-import com.novelsMDW.Novel.Repositories.AuthorRepository;
 import com.novelsMDW.Novel.Repositories.BookRepository;
 import com.novelsMDW.Novel.requests.BookRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
-    @Autowired
+
     private BookRepository bookRepository;
 
     @Autowired
-    private AuthorRepository authorRepository;
-
-    @Autowired
-    private AuthorService authorService;
-
+    public BookService(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
     public Book addBook(BookRequest bookRequest){
         Book book = new Book();
         book.setName(bookRequest.getName());
@@ -34,16 +29,10 @@ public class BookService {
         book.setPublishDate(bookRequest.getPublishDate());
         book.setPage(bookRequest.getPage());
 
-        Author author = authorRepository.findById(bookRequest.getAuthorId())
-                .orElseThrow(()
-                -> new AuthorNotFoundException("Author with ID: " + bookRequest.getAuthorId() + "Not Found"));
-        book.setAuthor(author);
         Book savedBook = bookRepository.save(book);
 
         return savedBook;
     }
-
-
 
     public Book getBookById(Long id){
         Book book = bookRepository.findById(id).get();
@@ -53,18 +42,20 @@ public class BookService {
         Book book = bookRepository.findById(id).get();
         bookRepository.delete(book);
     }
-
-
     public List<Book> getAllBooks(){
         return bookRepository.findAll();
     }
     public List<Book> getBooksByCategory(Category category){
+
         return bookRepository.findAllByCategory(category);
     }
 
     public List<Book> getBooksByAuthor(Author author){
+
         return bookRepository.findAllByAuthor(author);
     }
 
-    //ADD MORE SERVICES
+    public List<Book> findBooksByName(String name){
+        return  bookRepository.findBooksByName(name);
+    }
 }
